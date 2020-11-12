fa359629372514eb0b291cba9a52ef0435c37e08
Lukas Vavrek <lvavrek7@gmail.com>
2020-11-12 23:13:01 +0100

    SVD - bare minimum.

---
+++ svd/model.py
@@ -0,0 +1,37 @@
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from tensorflow.keras import optimizers
+
+from general.keras import disable_training_layers
+
+class SVDModel:
+    MAX_WIDTH = 60
+
+    def build_model(self):
+        # should we introduce constants or obtain this as a parameter?
+        n_mels = 128  * 2
+
+        conv_base = VGG16(
+            weights='imagenet',
+            include_top=False,
+            input_shape=(n_mels, self.MAX_WIDTH, 3))
+
+        disable_training_layers(conv_base)
+
+        flattened = layers.Flatten()(conv_base.output)
+        x = layers.Dense(16, activation='relu')(flattened)
+        x = layers.Dropout(rate=0.4)(x)
+        x = layers.Dense(8, activation='relu')(x)
+        x = layers.Dropout(rate=0.4)(x)
+        answer = layers.Dense(1, activation='sigmoid')(x) 
+
+        model = models.Model(conv_base.input, answer)
+
+        model.compile(
+            optimizer=optimizers.Adam(), #lr= 1e-6 0.001
+#           optimizer=optimizers.SGD(lr=0.1),
+            loss='binary_crossentropy',
+            metrics=['accuracy'])
+
+        return model
--- svd/preprocessor.py
+++ svd/preprocessor.py
@@ -1,13 +1,15 @@
 import numpy as np
 
-class PcGitaPreprocessor:
+class SVDPreprocessor:
     MAX_WIDTH = 60
 
     def preprocess(self, data):
         X_train = data.data["X_train"]
         X_val = data.data["X_val"]
+        X_test = data.data["X_test"]
         y_train = data.data["y_train"]
         y_val = data.data["y_val"]
+        y_test = data.data["y_test"]
 
         X_train = np.array([sample for subject in X_train for sample in self.process_subject(subject)])
         X_val = np.array([sample for subject in X_val for sample in self.process_subject(subject)])
@@ -17,9 +19,12 @@ class PcGitaPreprocessor:
 
         data.data["X_train"] = X_train
         data.data["X_val"] = X_val
+        data.data["X_test"] = X_test
         data.data["y_train"] = y_train
         data.data["y_val"] = y_val
+        data.data["y_test"] = y_test
 
+    # reusing methods from pcgita preprocessor (TODO: extract them out)
     """alternative to cut_and_stack_samples"""
     def process_subject(self, subject, width=MAX_WIDTH):
         x = []
@@ -40,3 +45,13 @@ class PcGitaPreprocessor:
             subject[1][:, :width],
             subject[2][:, :width]
         ], axis=2)
+
+    # including old methods, just in case
+    def all_samples_are_long_enough(self, sample):
+        return all(s.shape[1] >= 32 for s in sample)
+
+    def reshape_samples(self, sample):
+        return [np.asarray(s[:512, :32]) for s in sample]
+
+    def filter_short_data_samples(self, data):
+        return [self.reshape_samples(s) for s in data if self.all_samples_are_long_enough(s)]
